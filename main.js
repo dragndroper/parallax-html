b9dad1bc9293fd2a1ed6102cc8ff7ec7b0c33f43
avel-front <inbox.kalashnikov@gmail.com>
2022-05-18 23:24:31 +0300

    added parallax

---
+++ main.js
@@ -0,0 +1,18 @@
+new Rellax('.rellax');
+
+
+new Parallax(document.getElementById('scene1'))
+new Parallax(document.getElementById('scene2'))
+new Parallax(document.getElementById('scene3'))
+new Parallax(document.getElementById('scene4'))
+new Parallax(document.getElementById('scene5'))
+new Parallax(document.getElementById('scene6'))
+new Parallax(document.getElementById('scene7'))
+new Parallax(document.getElementById('scene8'))
+new Parallax(document.getElementById('scene9'))
+new Parallax(document.getElementById('scene10'))
+new Parallax(document.getElementById('scene11'))
+new Parallax(document.getElementById('scene12'))
+new Parallax(document.getElementById('scene13'))
+
+new Rellax('.parellax', {   horizontal: true    });
